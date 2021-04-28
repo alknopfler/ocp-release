6cd5ea02026c9d938f80ff8d64a3ff290f2c3ef7
Alberto Morgante <alknopfler@gmail.com>
2021-04-28 18:17:12 +0200

    working with main function

--- main.go
+++ main.go
@@ -9,51 +9,71 @@ import (
 
 const (
 	ACCEPTED = "Accepted"
-)
-var(
-	ref, status, row []string
-	rows [][]string
+	SUCCESS  = " Succeeded"
+	NONE     = "None"
 )
 
-func postScrape() {
+var (
+	ref, status, row []string
+	rows             [][]string
+	ex               bool
+)
+
+func postScrape(cond string) string {
 
 	doc, err := goquery.NewDocument("https://openshift-release.apps.ci.l2s4.p1.openshiftapps.com/#4.8.0-0.nightly")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	doc.Find("body > div > div.row > div > table:nth-child(10) > tbody").Each(func(index int, tablehtml *goquery.Selection) {
+		tablehtml.Find("tr").Each(func(indextr int, rowhtml *goquery.Selection) {
+			rowhtml.Find("td").Each(func(indexth int, tablecell *goquery.Selection) {
+				row = append(row, tablecell.Text())
+				if indexth == 0 {
+					ref = append(ref, tablecell.Text())
+				}
+				if indexth == 1 {
+					status = append(status, tablecell.Text())
+				}
 
-		doc.Find("body > div > div.row > div > table:nth-child(10) > tbody").Each(func(index int, tablehtml *goquery.Selection) {
-			tablehtml.Find("tr").Each(func(indextr int, rowhtml *goquery.Selection) {
-				rowhtml.Find("td").Each(func(indexth int, tablecell *goquery.Selection) {
-					row = append(row, tablecell.Text())
-					if indexth == 0 {
-						ref = append(ref, tablecell.Text())
-					}
-					if indexth == 1 {
-						status = append(status, tablecell.Text())
-					}
+			})
+			rows = append(rows, row)
+			row = nil
+		})
+	})
+	//log.Println("####### headings = ", len(ref), ref)
+	//log.Println("####### status = ", len(status), status)
+	//log.Println("####### rows = ", len(rows), rows)
+
+	for i, v := range status {
+
+		if v == ACCEPTED {
+			//log.Println("selected -----> " + ref[i])
+
+			doc2, err := goquery.NewDocument("https://openshift-release.apps.ci.l2s4.p1.openshiftapps.com/releasestream/4.8.0-0.nightly/release/" + ref[i])
+			if err != nil {
+				log.Fatal(err)
+			}
+			doc2.Find("body > div > ul:nth-child(7) > li:nth-child(2) > ul").Each(func(index int, tablehtml *goquery.Selection) {
+				tablehtml.Find("li").Each(func(indextr int, rowhtml *goquery.Selection) {
+					rowhtml.Find(".text-success").Each(func(indexth int, tablecell *goquery.Selection) {
+						if tablecell.Text() == cond+SUCCESS {
+							//log.Println("------encontracdo----> " + tablecell.Text())
+							ex = true
+						}
+					})
 
 				})
-				rows = append(rows, row)
-				row = nil
 			})
-		})
-		fmt.Println("####### headings = ", len(ref), ref)
-		fmt.Println("####### status = ", len(status), status)
-		fmt.Println("####### rows = ", len(rows), rows)
-
-		for i, v := range status {
-
-			if v == ACCEPTED {
-				fmt.Println(ref[i])
+			if ex {
+				return ref[i]
 			}
 		}
+	}
+	return "None"
 }
 
-
-
-
 func main() {
-	postScrape()
+	fmt.Println(postScrape("metal-assisted"))
 }
